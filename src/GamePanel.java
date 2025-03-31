02000bce323536de5a4bcb949540c8d1b63f52a3
Caden France <160537486+cfrance3@users.noreply.github.com>
2025-03-30 20:39:48 -0500

    Game loop, update/draw functions, init player

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,6 +2,8 @@ import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 
 public class GamePanel extends JPanel implements Runnable {
@@ -12,11 +14,14 @@ public class GamePanel extends JPanel implements Runnable {
     public final int HEIGHT = (int) ge.getMaximumWindowBounds().getHeight();
     public final int FPS = 120;
 
+    Player player;
+
     Thread gameThread;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.GRAY);
+        initPlayer();
         setDoubleBuffered(true);
         setFocusable(true);
     }
@@ -26,9 +31,41 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();
     }
 
+    private void initPlayer() {
+        player = new Player(this);
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        player.draw(g2);
+    }
+
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
     }
 }
