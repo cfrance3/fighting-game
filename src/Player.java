499275301bd07d0722e319323dd5680e9e0763a5
Caden France <160537486+cfrance3@users.noreply.github.com>
2025-03-30 20:38:41 -0500

    initial player class commit

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Player {
+
+    private float x;
+    private float y;
+    private final int WIDTH = 10;
+    private final int HEIGHT = 10;
+
+    public Player(GamePanel gp) {
+        this.x = 500;
+        this.y = 300;
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.YELLOW);
+        g2.fillRect((int) x, (int) y, WIDTH, HEIGHT);
+    }
+}
