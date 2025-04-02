a28ddfd1e2658892198a971d297031e47b871b4f
Caden France <160537486+cfrance3@users.noreply.github.com>
2025-04-01 20:51:10 -0500

    added load save method

---
+++ src/utils/LoadSave.java
@@ -0,0 +1,33 @@
+package utils;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class LoadSave {
+
+    public static final String PLAYER_RIGHT = "res/blankFighterIdleRight.png";
+    public static final String PLAYER_LEFT = "res/blankFighterIdleLeft.png";
+
+    public static BufferedImage getSpriteAtlas(String fileName) {
+
+        BufferedImage img = null;
+        InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+
+        try {
+            img = ImageIO.read(is);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return img;
+    }
+}
